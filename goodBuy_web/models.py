fadd81923148569363076023dce92d624fd450e5
11056012 <05162006keke@gmail.com>
2025-03-19 21:40:12 +0800

    user刪除註冊日期改用django內建

--- goodBuy_web/models.py
+++ goodBuy_web/models.py
@@ -5,7 +5,6 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     introduce = models.TextField(blank=True, null=True)
     img = models.ImageField(upload_to='user_images/', null=True, blank=True)
-    register_date = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
