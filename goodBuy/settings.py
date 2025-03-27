c2cc22c4ed346c044c5ff9211dcd3d7ab6818549
11056012 <05162006keke@gmail.com>
2025-03-27 21:45:22 +0800

    setting upload路徑設定 blacklist model創建 圖片上傳測試通過

--- goodBuy/settings.py
+++ goodBuy/settings.py
@@ -132,6 +132,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/upload/'
+MEDIA_ROOT = BASE_DIR / 'upload'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
