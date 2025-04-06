ca413020d043831b9d4d0c75b7765e5f0d8e1ed8
11056012 <05162006keke@gmail.com>
2025-04-06 22:06:43 +0800

    models拆

--- goodBuy_shop/models/shop_payment.py
+++ goodBuy_shop/models/shop_payment.py
@@ -1,7 +1,7 @@
 from django.db import models
 from .shop import Shop
-from goodBuy_web.models import Payment_Account
+from goodBuy_web.models import PaymentAccount
 
 class ShopPayment(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
-    payment_account = models.ForeignKey(Payment_Account, on_delete=models.CASCADE)
+    payment_account = models.ForeignKey(PaymentAccount, on_delete=models.CASCADE)
