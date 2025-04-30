d6a7deca71d96325499f2e4ff16456bf30a1f0f5
11056012 <05162006keke@gmail.com>
2025-05-01 01:47:00 +0800

    0430-資料庫整理

--- goodBuy_shop/models/shop_announcement.py
+++ goodBuy_shop/models/shop_announcement.py
@@ -8,7 +8,7 @@ class ShopAnnouncement(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     announcement = models.TextField(blank=True, null=True)
-    date = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
