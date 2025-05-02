712138fc4a62531af4cd9cea0a53fe0a109e401e
L1zn <prkwh0629@gmail.com>
2025-05-03 00:42:33 +0800

    debug + 註冊完善 + 讓setting 讀到webConfig set

--- goodBuy_web/signals.py
+++ goodBuy_web/signals.py
@@ -5,7 +5,8 @@ from goodBuy_web.models import *
 #在 User 創建時自動建立 Profile
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    print("signals.py tr")  
+
     if created:
         Profile.objects.create(user=instance)
-
-        print('signals.py loaded!')
+        print('✅ Profile auto-created')
