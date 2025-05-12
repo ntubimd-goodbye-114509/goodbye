3a85b487b84b5d170b9a36596bb381c5417703d4
L1zn <prkwh0629@gmail.com>
2025-05-12 23:57:48 +0800

    req檔案更新 ; home的js路徑更改 ; 購物車 ; edit_shop和shop_detail.html更新 ; time_detail多一個判斷

--- goodBuy_shop/time_utils.py
+++ goodBuy_shop/time_utils.py
@@ -7,7 +7,10 @@ from datetime import datetime
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M"
 # 時間格式轉換&NULL賦值
 def timeFormatChange_now(t):
-    if t:
+    # 如果已經是 datetime，就直接回傳
+    if isinstance(t, datetime):  
+        return t
+    elif t:
         return datetime.strptime(t, DATETIME_FORMAT)
     else:
         return datetime.strptime(datetime.now().strftime(DATETIME_FORMAT), DATETIME_FORMAT)
