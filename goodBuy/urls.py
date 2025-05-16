aee9b354e0425ba78d0a72667c27a67c7891a381
11056012 <05162006keke@gmail.com>
2025-05-16 19:50:50 +0800

    order_list小修正

--- goodBuy/urls.py
+++ goodBuy/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('order/action/', include('goodBuy_order.urls.order_action')),
     path('order/payment/', include('goodBuy_order.urls')),
     path('cart/', include('goodBuy_order.urls.cart')),
-   
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- goodBuy_order/urls/payment.py
+++ goodBuy_order/urls/payment.py
@@ -2,6 +2,7 @@ from django.urls import path
 from goodBuy_order.views import *
 
 urlpatterns = [
+    path('payment_records/', my_payment_records, name='my_payment_records'),
     path('order/<int:order_id>/', view_order_payment_history, name='view_payment_proofs'),
     path('list/', list_related_payments, name='list_related_payments'),
     path('second_supplement/<int:order_id>/', set_second_supplement, name='set_second_supplement'),
