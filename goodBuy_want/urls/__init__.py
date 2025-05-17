90cfa29ca9c43ea5a204b39a38c8146a335c566f
11056012 <05162006keke@gmail.com>
2025-05-17 21:53:46 +0800

    urls init

--- goodBuy_want/urls/__init__.py
+++ goodBuy_want/urls/__init__.py
@@ -1,7 +1,5 @@
 from .want import urlpatterns as want_urlpatterns
 from .want_action import urlpatterns as action_urlpatterns
 
-urlpatterns = (
-    want_urlpatterns +
-    action_urlpatterns
-)
+urlpatterns = want_urlpatterns + action_urlpatterns
+
