a0576035b1b549615a046e1ff402d3866f7c64e9
11056012 <05162006keke@gmail.com>
2025-05-17 21:52:29 +0800

    shop urls修正

--- goodBuy_shop/urls/__init__.py
+++ goodBuy_shop/urls/__init__.py
@@ -2,8 +2,5 @@ from .shop import urlpatterns as shop_urlpatterns
 from .shop_action import urlpatterns as action_urlpatterns
 from .user import urlpatterns as user_urlpatterns
 
-urlpatterns = (
-    shop_urlpatterns +
-    action_urlpatterns +
-    user_urlpatterns
-)
+urlpatterns = shop_urlpatterns + action_urlpatterns + \
+            user_urlpatterns
