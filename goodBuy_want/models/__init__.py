e1e3722208bf7fffd62a61525f427c1c147a3983
11056012 <05162006keke@gmail.com>
2025-06-22 01:16:32 +0800

    want推薦記錄table

--- goodBuy_want/models/__init__.py
+++ goodBuy_want/models/__init__.py
@@ -3,3 +3,4 @@ from .want_back import WantBack
 from .want_footprints import WantFootprints
 from .want_tag import WantTag
 from .want_img import WantImg
+from .want_recommendationHistory import WantRecommendationHistory
