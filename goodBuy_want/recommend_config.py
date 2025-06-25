4cb59630bd20b937d51e46f4c4b4ad514c2c4f82
11056012 <05162006keke@gmail.com>
2025-06-25 15:06:42 +0800

    權重變數修改

--- goodBuy_want/recommend_config.py
+++ goodBuy_want/recommend_config.py
@@ -1,12 +1,12 @@
 # -------------------------
 # 熱門 Want 推薦權重
 # -------------------------
-WANT_HOT_WEIGHTS = {
+HOT_WEIGHTS = {
     'recent_views': 1,     # 每次被瀏覽 +1
     'recent_replies': 5,   # 每次被回覆 +5
 }
 
-WANT_HOT_PROPORTIONS = {
+HOT_PROPORTIONS = {
     'recent_views': 0.4,
     'recent_replies': 0.6,
 }
@@ -14,13 +14,13 @@ WANT_HOT_PROPORTIONS = {
 # -------------------------
 # 個性化 Want 推薦權重
 # -------------------------
-WANT_PERSONAL_WEIGHTS = {
+PERSONAL_WEIGHTS = {
     'search_keyword': 4,              # 搜尋關鍵字匹配
     'viewed_related_multiplier': 0.7, # 觀看過相關 want 的關鍵字提取
     'replied_related_bonus': 2,       # 回覆過相關 want 的 tag 關聯加分
 }
 
-WANT_PERSONAL_PROPORTIONS = {
+PERSONAL_PROPORTIONS = {
     'search_history': 0.4,
     'viewed_related': 0.4,
     'replied_related': 0.2,
@@ -29,13 +29,13 @@ WANT_PERSONAL_PROPORTIONS = {
 # -------------------------
 # Want 關鍵字匹配加權
 # -------------------------
-WANT_KEYWORD_SCORES = {
+KEYWORD_SCORES = {
     'tags': 5,
     'title': 3,
     'post_text': 2,
 }
 
-WANT_KEYWORD_PROPORTIONS = {
+KEYWORD_PROPORTIONS = {
     'tags': 0.5,
     'title': 0.3,
     'post_text': 0.2,
@@ -49,6 +49,6 @@ RECOMMENDED_WANT_WEIGHT_MULTIPLIER = 0.5
 # -------------------------
 # 查詢時間範圍
 # -------------------------
-WANT_SEARCH_HISTORY_DAYS = 3
-WANT_VIEW_DAYS = 14
-WANT_REPLY_DAYS = 14
+SEARCH_HISTORY_DAYS = 3
+VIEW_DAYS = 14
+REPLY_DAYS = 14
