75416898424c5e052297a7b688367bd6008a714e
L1zn <prkwh0629@gmail.com>
2025-07-02 23:17:58 +0800

    shop 新增url

--- goodBuy_shop/urls/shop.py
+++ goodBuy_shop/urls/shop.py
@@ -8,6 +8,16 @@ urlpatterns = [
     path('<int:shop_id>/edit/', edit_shop, name='shop_edit'),
     path('<int:shop_id>/delete/', deleteShop, name='shop_delete'),
 
+    #搜尋
     path('shop/search/', shopBySearch, name='shop_search'),
     path('shop/search/user/<int:user_id>/', shopBySearch, name='shop_search_by_user'),
+
+    #公告
+    path('<int:shop_id>/announcement/add/', addAnnouncement, name='add_announcement'),
+    path('announcement/<int:announcement_id>/edit/', editAnnouncement, name='edit_announcement'),
+    path('announcement/<int:announcement_id>/delete/', deleteAnnouncement, name='delete_announcement'),
+
+    #公告列表(單一及多筆)
+    path('<int:shop_id>/announcements/', showShopAnnouncement_many, name='shop_announcement_list'),
+    path('<int:shop_id>/announcement/<int:announcement_id>/', showShopAnnouncement_one, name='show_announcement'),
 ]
