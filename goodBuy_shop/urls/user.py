4c02ce6172975d93bacc6d734ee19e8285aa531c
L1zn <prkwh0629@gmail.com>
2025-07-14 23:51:10 +0800

    收藏界面

--- goodBuy_shop/urls/user.py
+++ goodBuy_shop/urls/user.py
@@ -3,4 +3,5 @@ from goodBuy_shop.views import *
 
 urlpatterns = [
     path('<int:shop_id>/collect/', shop_collect_toggle, name='shop_collect_toggle'),
+    path('shop/collects/', my_shops_collected, name='my_shops_collected'),
 ]
